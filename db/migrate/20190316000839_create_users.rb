937df316abccc95cea361abfacb6afb480416b52
shin1rok <skssikm@gmail.com>
2019-03-16 09:19:32 +0900

    db:migrate

--- db/migrate/20190316000839_create_users.rb
+++ db/migrate/20190316000839_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
-      t.integer :gender
-      t.date :birthday
+      t.string :name, null: false
+      t.integer :gender, null: false
+      t.date :birthday, null: false
       t.integer :prefecture_id
       t.text :profile
 
